043cc8e08dec8f7c704f5584eec9bc5cb30fd060
Zevi Liao <jointjm@gmail.com>
2020-10-29 06:52:43 +0800

    update

--- src/rxcode/sub.ts
+++ src/rxcode/sub.ts
@@ -1,16 +1,20 @@
 import { defer, Observable, of, Subject, ConnectableObservable } from "rxjs";
-import { multicast, refCount } from "rxjs/operators";
+import { multicast, refCount, delay } from "rxjs/operators";
 
 
 export const rxTest = () => {
-  const source = defer(() =>
-    of(Math.floor(Math.random() * 100))
-  );
+  // const source = defer(() =>
+  //   of(Math.floor(Math.random() * 100))
+  // );
+  const source = defer(() => of(Math.floor(Math.random() * 100))
+    .pipe(delay(0)));
 
-  const m = source.pipe(multicast(new Subject<number>()), refCount()) as ConnectableObservable<number>;
+  const m = source.pipe(
+    multicast(() => new Subject<number>()),
+    refCount()
+  );
   m.subscribe(observer("a"));
   m.subscribe(observer("b"));
-  // m.connect();
 }
 
 

92e91f6cac157bbf565265c0cc1f851cff7b770e
Zevi Liao <jointjm@gmail.com>
2020-11-04 00:05:03 +0800

    update

--- src/rxcode/sub.ts
+++ src/rxcode/sub.ts
@@ -1,28 +1,19 @@
-import { Subject, concat, defer, Observable, of, ConnectableObservable, timer, Observer, from } from "rxjs";
-import { delay, publish, switchMapTo, publishBehavior, publishLast, share } from "rxjs/operators";
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+(global as any).WebSocket = require('ws');
 
 
 export const rxTest = () => {
+  const ws = webSocket('wss://echo.websocket.org');
 
-  // Subject
-  let sj = new Subject<string>() // subject
+  ws.subscribe(res => {
+    console.log('message', res);
+  });
 
-  let orA = (v: any) => console.log('a', v) // observer favor topic
-  let orB = (v: any) => console.log('b', v)
+  ws.next(`content`);
+  ws.next(`content`);
+  ws.next(`content`);
 
-  // sj.subscribe(or) // sub
-  sj.subscribe(orA) // topic A
-  sj.subscribe(orB) // topic B
-
-  // let ob2 = from(['s1', 's2']) // array pub and data source
-  let ob2 = of('s1', 's2') // iterator pub and data source
-
-  ob2.subscribe(sj) // observable be subscribed by subject-(topics)  -- execute (binding)
-
-  // sj.subscribe(or)
-  // sj.subscribe(or)
-  // sj.next('s1')  // one by one data source
-  // sj.next('s2')
+  ws.complete();
 
 }
 
